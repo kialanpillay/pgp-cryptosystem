09c03c5f969586f7f7a8e12a75aa6d6528938a43
aidan <BLYAID001@myuct.ac.za>
2021-06-09 13:46:13 +0200

    Add sha256 hash method

--- src/main/java/PGP.java
+++ src/main/java/PGP.java
@@ -1,4 +1,12 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PGP {
-
+    public static String hash(String message) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(
+                message.getBytes(StandardCharsets.UTF_8));
+        return new String(encodedhash);
+    }
 }
