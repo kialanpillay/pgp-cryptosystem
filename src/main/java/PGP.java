a62c5c2a111ec7be0cc82b1fbb0f308836fae013
aidan <BLYAID001@myuct.ac.za>
2021-06-09 21:05:00 +0200

    Add aes methods

--- src/main/java/PGP.java
+++ src/main/java/PGP.java
@@ -1,13 +1,11 @@
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.zip.*;
 
 public class PGP {
-
-    // Can this be fixed or does it need to be set for each session key?
-    private static final IvParameterSpec IV = generateIV();
 
     public static byte[] RSAEncryption(byte[] messageBytes, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
@@ -23,16 +21,16 @@ public class PGP {
         return decryptedMessageBytes;
     }
 
-    public static byte[] AESEncryption(byte[] messageBytes, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    public static byte[] AESEncryption(byte[] messageBytes, Key key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key, IV);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
         byte[] encryptedMessageBytes = cipher.doFinal(messageBytes);
         return encryptedMessageBytes;
     }
 
-    public static byte[] AESDecryption(byte[] encryptedMessageBytes, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    public static byte[] AESDecryption(byte[] encryptedMessageBytes, Key key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key, IV);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
         byte[] decryptedMessageBytes = cipher.doFinal(encryptedMessageBytes);
         return decryptedMessageBytes;
     }
