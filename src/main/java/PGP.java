8d2d494a799ff92d44c10fe1efb4c5e8472ef8d9
Emily <mrremi007@myuct.ac.za>
2021-06-09 14:29:44 +0200

    Implemented RSA encryption method

--- src/main/java/PGP.java
+++ src/main/java/PGP.java
@@ -1,6 +1,9 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 
 public class PGP {
     public static String hash(String message) throws NoSuchAlgorithmException {
@@ -8,5 +11,25 @@ public class PGP {
         byte[] encodedhash = digest.digest(
                 message.getBytes(StandardCharsets.UTF_8));
         return new String(encodedhash);
+    }
+
+    public static String rsaEncryption(String message, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+        return new String(encryptedMessageBytes);
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        String toEcnrypt = "Yes, it works";
+        System.out.println(toEcnrypt);
+//        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+//        generator.initialize(2048);
+//        KeyPair pair = generator.generateKeyPair();
+        KeyPair pair = KeyGenerator.generate();
+        System.out.println("startting RSA");
+        String encrypted = rsaEncryption(toEcnrypt, pair.getPrivate());
+        System.out.println(encrypted);
     }
 }
