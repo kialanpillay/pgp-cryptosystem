ebe8a73e22fed574fa1f37124813870a734f03ed
Emily <mrremi007@myuct.ac.za>
2021-06-09 14:29:44 +0200

    Implemented RSA encryption method

--- src/main/java/PGP.java
+++ src/main/java/PGP.java
@@ -1,6 +1,9 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 
 public class PGP {
     public static String hash(String message) throws NoSuchAlgorithmException {
@@ -8,5 +11,13 @@ public class PGP {
         byte[] encodedhash = digest.digest(
                 message.getBytes(StandardCharsets.UTF_8));
         return new String(encodedhash);
+    }
+
+    public static String RSAEncryption(String message, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+        return new String(encryptedMessageBytes);
     }
 }
